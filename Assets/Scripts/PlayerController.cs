c7345008d5d2d444b0e4db99da8c55ee5d8c8675
Petter Björklund <petter@hemvist.se>
2018-11-12 13:27:24 +0100

    First

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float groundSpeed;
+    public float airSpeed;
+
+    public float maxBoost; //Andel av bashastigheten som läggs till med maxBoost
+    private float boost; //Hur mycket boost man har
+    public float boostDecaySpeed; //Hur snabbt boosten ändras
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0f));
+    }
+}
